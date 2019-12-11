d1731b9f10d82aa188d49e0610cc3451d15f14a1
jf781 <50719731+jf781@users.noreply.github.com>
2019-12-11 15:45:09 -0600

    Updated help file

--- Azure/Internal/Confirm-ModulesInstalled.ps1
+++ Azure/Internal/Confirm-ModulesInstalled.ps1
@@ -2,12 +2,41 @@ function Confirm-ModulesInstalled {
     [CmdLetBinding()]
     param (
         [Parameter(
-            Mandatory = $false,
+            Mandatory = $true,
             ValueFromPipeline = $true
         )]
         [string[]]
         $modules
     )
+    <#
+    .SYNOPSIS
+        This script will check to see if the modules supplied via the $modules parameter are installed on the system.  It will provide an out of all modules and if they are installed.  
+    .DESCRIPTION
+        This script does not install or make any changes.  It only checks to see if the modules are installed. 
+    .PARAMETER Modules
+            Please provide one or more modules that you wish to check if installed.  If there are multiples please seperate by a comma
+    .INPUTS
+        Requires the $module parameter to be populated with one or more items
+    .OUTPUTS
+        Outputs a list two columns.  ModuleName and Installed.  ModuleName will display the name of the module and Installed will display True or False depending if that module is installed on the system
+    
+    .NOTES
+        Version:        1.0
+        Author:         Joe Fecht - AHEAD, llc.
+        Creation Date:  December 2019
+        Purpose/Change: Initial deployment
+    
+    .EXAMPLE
+        Confirm-ModulesInstalled -modules az.resources,az.accounts
+
+        Checks for the modules Az.Resources and Az.Accounts
+        Ouput shows the Az.Resources module is not installed but Az.Accounts is installed on the system. 
+
+        ModuleName   Installed
+        ----------   ---------
+        az.accounts       True
+        az.resources     False
+    #>
     PROCESS {
         Write-Verbose "Testing if Modules are installed"
         $results = @()
@@ -41,6 +70,3 @@ function Confirm-ModulesInstalled {
         return $results
     }
 }
-
-# Example
-# Confirm-ModulesInstalled -modules Az.Accounts,Az.Resources
